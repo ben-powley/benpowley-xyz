fc353bffdffb11657d22caae7f64a0272c4f14a3
Ben Powley <ben.powley97@gmail.com>
2024-02-22 23:56:07 +0000

    Responsive adjustments

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        customPulse: "customPulse 5s linear infinite",
+      },
+      keyframes: {
+        customPulse: {
+          "0%, 100%": { opacity: 1 },
+          "50%": { opacity: 0.5 },
+        },
+      },
+    },
   },
   plugins: [],
 };
