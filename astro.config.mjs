a7959717084ce7fb554a2c39b1d4729857db865e
Ben Powley <ben.powley97@gmail.com>
2024-02-24 13:40:28 +0000

    Add compression. Do not lazy load above the fold image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
-import { defineConfig, passthroughImageService  } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import netlify from "@astrojs/netlify";
+
+import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), compressor()],
   output: "server",
   adapter: netlify(),
-  image: {service: passthroughImageService()}
+  image: {
+    service: passthroughImageService()
+  }
 });
